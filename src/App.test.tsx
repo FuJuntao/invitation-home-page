c31142889a976b93303c12a1969858af61e33187
FuJuntao <fu.juntao358@gmail.com>
2021-03-10 16:03:24 +0800

    feat: add dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,9 +4,9 @@ import { expect } from 'chai';
 import App from './App';
 
 describe('<App>', () => {
-  it('renders learn react link', () => {
+  it('renders slogan', () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
+    const linkElement = getByText('A better way to enjoy every day.');
     expect(document.body.contains(linkElement));
   });
 });
---
+++ src/HomePage/index.tsx
@@ -0,0 +1,14 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import { Box, Heading, Text } from '@chakra-ui/layout';
+import React from 'react';
+
+function HomePageIndex() {
+  return (
+    <Box>
+      <Heading as="h1">A better way to enjoy every day.</Heading>
+    </Box>
+  );
+}
+
+export default HomePageIndex;
