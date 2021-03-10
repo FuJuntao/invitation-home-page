d3d237a5d687a10eec84fc01685f5056141a4fa2
FuJuntao <fu.juntao358@gmail.com>
2021-03-11 00:43:05 +0800

    feat: add inputs

--- src/InvitationForm/index.tsx
+++ src/InvitationForm/index.tsx
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 import { Button } from '@chakra-ui/button';
+import { FormControl, FormLabel } from '@chakra-ui/form-control';
+import { Input } from '@chakra-ui/input';
 import {
   ModalBody,
   ModalCloseButton,
@@ -12,11 +14,27 @@ import { jsx } from '@emotion/react';
 function InvitationForm() {
   return (
     <ModalContent>
-      <ModalHeader>Request an invite</ModalHeader>
+      <ModalHeader textAlign="center">Request an invite</ModalHeader>
       <ModalCloseButton />
-      <ModalBody>form</ModalBody>
 
-      <ModalFooter>
+      <ModalBody>
+        <FormControl>
+          <FormLabel htmlFor="fullName">Full name</FormLabel>
+          <Input id="fullName" placeholder="Full name" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <Input id="email" type="email" placeholder="Email" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="confirmEmail">Confirm Email</FormLabel>
+          <Input id="confirmEmail" type="email" placeholder="Confirm Email" />
+        </FormControl>
+      </ModalBody>
+
+      <ModalFooter justifyContent="center">
         <Button>Send</Button>
       </ModalFooter>
     </ModalContent>
