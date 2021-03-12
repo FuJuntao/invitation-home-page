ca55a7179ed7ad77d8b526f21cece4c0fe98126b
FuJuntao <fu.juntao358@gmail.com>
2021-03-12 18:09:33 +0800

    feat: add test for header

---
+++ src/Header/Header.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { expect } from 'chai';
+import { companyName } from '../shared/texts';
+import Header from './Header';
+
+describe('<Header/>', () => {
+  it('renders company name', () => {
+    render(<Header />);
+    expect(screen.getAllByText(companyName)).not.null;
+  });
+});
--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -1,16 +1,3 @@
-/** @jsx jsx */
-import { Box, Heading } from '@chakra-ui/react';
-import { jsx } from '@emotion/react';
-import { companyName } from '../shared/texts';
-
-function Header() {
-  return (
-    <Box as="header">
-      <Heading as="h2" size="sm">
-        {companyName}
-      </Heading>
-    </Box>
-  );
-}
+import Header from './Header';
 
 export default Header;
