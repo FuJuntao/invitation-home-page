ba3361a199efa5ad6d4430b114fb26fc2940c4bf
FuJuntao <fu.juntao358@gmail.com>
2021-03-16 00:06:31 +0800

    feat: update tests

--- src/Footer/Footer.test.tsx
+++ src/Footer/Footer.test.tsx
@@ -5,11 +5,15 @@ import Footer from './Footer';
 describe('<Footer/>', () => {
   it('renders created place', () => {
     render(<Footer />);
-    screen.getByRole('heading', { name: /shanghai/i });
+    expect(screen.getByRole('contentinfo')).toContainElement(
+      screen.getByRole('heading', { name: /shanghai/i }),
+    );
   });
 
   it('renders rights', () => {
     render(<Footer />);
-    screen.getByRole('heading', { name: /all rights reserved/i });
+    expect(screen.getByRole('contentinfo')).toContainElement(
+      screen.getByRole('heading', { name: /all rights reserved/i }),
+    );
   });
 });
