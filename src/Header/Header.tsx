08657a4a9d49b7ea017ae6d9bb93597a5d1d5ea4
FuJuntao <fu.juntao358@gmail.com>
2021-03-15 23:11:12 +0800

    feat: add styles for main

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -5,7 +5,7 @@ import { companyName } from '../shared/texts';
 function Header() {
   return (
     <Box as="header" mx="4" py="4" borderBottom="1px" borderColor="gray.200">
-      <Heading as="h2" size="md" color="twitter.500">
+      <Heading as="h2" size="md" color="teal.400">
         {companyName}
       </Heading>
     </Box>
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -9,14 +9,16 @@ function Main() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Center as="main" flex="1" flexDirection="column">
-      <Heading as="h1" textAlign="center">
+    <Center as="main" flex="1" flexDirection="column" p="4" textAlign="center">
+      <Heading as="h1" size="2xl">
         {slogan}
       </Heading>
-      <Heading as="h2" size="md" textAlign="center">
+      <Heading as="h2" size="md" mt="12" color="teal.400">
         Be the first to know when we launch.
       </Heading>
-      <Button onClick={onOpen}>Request an invite</Button>
+      <Button mt="6" colorScheme="teal" onClick={onOpen}>
+        Request an invite
+      </Button>
 
       <InvitationFormModal isCentered isOpen={isOpen} onClose={onClose} />
     </Center>
